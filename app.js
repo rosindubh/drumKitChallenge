66493e3858612f24643e0f20ffb5b8a84f954712
rosindubh <p.welsby2@ntlworld.com>
2021-08-11 13:10:24 +0100

    added comments, Hi-Hat and Kick, corrected Kick name

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+/* phil welsby - 11 aug 2021 Drum Kit Challenge*/
 document.addEventListener('keydown', function(e) {
     if (e.keyCode == 65) {
       document.getElementById('boom').play();
     }
     else if (e.keyCode == 83) {
       document.getElementById('clap').play();
+    }
+    else if (e.keyCode == 68) {
+      document.getElementById('hihat').play();
+    }
+    else if (e.keyCode == 70) {
+      document.getElementById('kick').play();
     }
   });
---
+++ list_of_key_numbers.md
@@ -0,0 +1,12 @@
+# phil welsby 11 aug 2021
+# list of key number for the CodeNation Drum Kit Challenge
+
+A = 65 - Boom
+S = 83 - Clap
+D = 68
+F = 70
+G = 71
+H = 72
+J = 74
+K = 75
+L = 76
