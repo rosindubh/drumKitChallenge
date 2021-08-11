b166c9fc8e4f6e06df3422346ec61498ea634f9a
rosindubh <p.welsby2@ntlworld.com>
2021-08-11 12:16:21 +0100

    added clap

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ document.addEventListener('keydown', function(e) {
     if (e.keyCode == 65) {
       document.getElementById('boom').play();
     }
+    else if (e.keyCode == 83) {
+      document.getElementById('clap').play();
+    }
   });
