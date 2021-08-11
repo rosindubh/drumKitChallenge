64ff941698144486b244119a1644df1fff356998
rosindubh <p.welsby2@ntlworld.com>
2021-08-11 13:29:57 +0100

    added more instruments

--- app.js
+++ app.js
@@ -12,4 +12,16 @@ document.addEventListener('keydown', function(e) {
     else if (e.keyCode == 70) {
       document.getElementById('kick').play();
     }
+    else if (e.keyCode == 71) {
+      document.getElementById('openhat').play();
+    }
+    else if (e.keyCode == 72) {
+      document.getElementById('ride').play();
+    }
+    else if (e.keyCode == 74) {
+      document.getElementById('snare').play();
+    }
+    else if (e.keyCode == 75) {
+      document.getElementById('tink').play();
+    }
   });
