abe6c8570765fd271b85ab5f9d0c3654792c0abe
rosindubh <p.welsby2@ntlworld.com>
2021-08-27 14:31:40 +0100

    deleted a line then added back

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /* phil welsby - 11 aug 2021 Drum Kit Challenge*/
 document.addEventListener('keydown', function(e) {
+
     if (e.keyCode == 65) {
-      document.getElementById('boom').play();
+      document.getElementById
+      ('boom').play();
     }
     else if (e.keyCode == 83) {
       document.getElementById('clap').play();
